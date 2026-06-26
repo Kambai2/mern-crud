e6c32df384306f72b322505ae955be004534ba36
kmiracles <miraclekambai2@gmail.com>
2026-06-26 12:28:46 +0100

    Use dynamic import in api/index.js to load ESM server/app.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import app, { connectToDatabase } from '../server/app.js';
-
 export default async function handler(req, res) {
+  const mod = await import('../server/app.js');
+  const app = mod.default;
+  const connectToDatabase = mod.connectToDatabase;
   await connectToDatabase();
   return app(req, res);
 }
